bb12fef5c7874dbe03da32b1229bf1edd6055702
suraj <surajkumarsahani1997@gmail.com>
2021-08-24 08:52:50 +0530

    first commit

---
+++ app/src/main/java/martialcoder/surajsahani/spacexcrew/MainActivity.java
@@ -0,0 +1,14 @@
+package martialcoder.surajsahani.spacexcrew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
